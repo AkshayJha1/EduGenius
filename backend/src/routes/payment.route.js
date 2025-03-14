6d2040b1a59a889718206824044ea10effe78b31
Akshay Kumar Jha <akshaykumarjha26@gmail.com>
2025-03-14 11:46:06 +0530

    fixing payment controller A3

--- backend/src/routes/payment.route.js
+++ backend/src/routes/payment.route.js
@@ -5,7 +5,7 @@ const { authenticateUser } = require('../middlewares/authenticateUser.middleware
 
 const { checkOut, handlingWebhook } = require('../controllers/payment.controller');
 
-router.route('/checkOut').post(authenticateUser, checkOut);
+router.post('/checkOut', express.json() , authenticateUser, checkOut)
 router.post("/handlingWebhook", express.raw({ type: "application/json" }), handlingWebhook);
 
 module.exports = router;
