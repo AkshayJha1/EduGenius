347c8812a2a341a1cd36b4132190534ab1f2f02e
Akshay Kumar Jha <akshaykumarjha26@gmail.com>
2025-03-14 09:57:10 +0530

    comtinnnnng

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",  // Ensure build output is in "dist"
+    assetsDir: "assets",  // Keep assets in "assets" folder
+  },
+  server: {
+    historyApiFallback: true, // Important for React Router
+  }
 })
